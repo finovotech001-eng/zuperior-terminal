f1aa1ec6c4902abac191b61d53319d84e01fb57e
Akhilesh Lakhanpal <akhileshlakhanpal660@gmail.com>
2025-10-22 19:36:40 +0530

    Big Fixing

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,8 @@
-import { PrismaClient } from './generated/prisma';
+import { PrismaClient } from '@prisma/client';
+
 
 // PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
 // exhausting your database connection limit.
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
